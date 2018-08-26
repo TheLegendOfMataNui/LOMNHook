447a8dbccff77b45685ab48b560db4e100e1b7c5
Liam Scott <vahkiti@maskofdestiny.com>
2018-08-26 04:25:48 -0400

    Fixed an issue involving an external plugin dependancy. Changed default build dir.

--- LOMNHook/Mods/FrameThrottle.h
+++ LOMNHook/Mods/FrameThrottle.h
@@ -6,7 +6,7 @@ struct FrameThrottle : ScProcess {
 public:
 	FrameThrottle();
 	void Process(double time, double deltaTime);
-	float TargetFrameTime = 1.0f / 10.0f;
+	float TargetFrameTime = 1.0f / 60.0f;
 	bool Enabled = true;
 
 private:
