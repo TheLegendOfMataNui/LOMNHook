e2d2698355134ccd08535f266af8a5251fb6a5a8
Ben Garcia <43491016+bgarcia17@users.noreply.github.com>
2019-03-14 12:08:36 -0700

    Add test data

--- LcDashboard/dashboard-client/js/lcdashboard.js
+++ LcDashboard/dashboard-client/js/lcdashboard.js
@@ -6,10 +6,119 @@ const tableTreeElement = document.getElementById("tabletree");
 const canvasElement = document.getElementById("canvas");
 
 const apiIndexEndpoint = "/api/index";
+const apiTableEndpoint = "/api/table";
+const widgetTypes = { label: {create: labelContentCreate, update: labelContentUpdate} };
 
 var treeItems = { }; // Dictionary<string, TableTreeItem>
-var tables = { }; // Dictionary<string, Table>
 var selectedTreeItem = null; // TableTreeItem
+var currentTableContents = null; // TableContentsControl
+var loadedTables = { }; // Dictionary<string, Table>
+
+function labelContentCreate(widgetData) {
+
+}
+
+function labelContentUpdate(contentElement, widgetData) {
+
+}
+
+function placeWidgetControl(control, row, column) {
+    control.row = row;
+    control.column = column;
+    // TODO: Set CSS grid & column with inline style
+}
+
+function buildWidgetElement(name, contentElement) {
+    let label = document.createElement("DIV");
+    label.className = "widget-label";
+    label.appendChild(document.createTextNode(name));
+    
+    let content = document.createElement("DIV");
+    content.className = "widget-content";
+    content.appendChild(contentElement);
+
+    let widget = document.createElement("DIV");
+    widget.className = "widget";
+    widget.appendChild(label);
+    widget.appendChild(content);
+    return widget;
+}
+
+//
+// WidgetControl: A single widget in the UI. Takes its data from a WidgetInfo.
+//
+function WidgetControl(widgetInfo, row, column) {
+    this.info = widgetInfo;
+    this.info.controls.push(this);
+    this.contentElement = this.info.type.create(this.info.data);
+    this.element = buildWidgetElement(this.info.name, this.contentElement);
+    this.row = row;
+    this.column = column;
+    placeWidgetControl(this, row, column);
+}
+
+//
+// TableContentsControl
+//
+function TableContentsControl() {
+    this.widgets = []; // WidgetControl[]
+    this.element = document.createElement("DIV");
+    this.element.className = "table-contents";
+    this.widgetCount = 0;
+}
+
+//
+// WidgetInfo: Stores the data for a single widget.
+//
+function WidgetInfo(name, typeName, data) {
+    this.name = name;
+    this.typeName = typeName;
+    this.type = widgetTypes[typeName];
+    this.data = data;
+    this.controls = []; // WidgetControl[]
+}
+
+//
+// TableInfo: Stores the data for a table and the contained widgets.
+//
+function TableInfo(name) {
+    this.name = name;
+    this.widgets = { }; // Map<string, Widget>
+    this.controls = []; // TableContentsControl[]
+}
+
+function loadTableData(table) {
+    fetch(apiTableEndpoint + "/" + table.name).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        if (json.success) {
+            let oldWidgets = table.widgets;
+            table.widgets = { };
+            for (let i = 0; i < json.data.widgets.length; i++) {
+                let widgetData = json.data.widgets[i];
+                let widget = null;
+                if (widgetData.name in oldWidgets) {
+                    // Update existing widget
+                    widget = oldWidgets[widgetData.name];
+                    delete oldWidgets[widgetData.name];
+                }
+                else {
+                    // Create new widget
+                    widget = new WidgetControl(widgetData);
+                }
+
+            }
+
+            for (let i = 0; i < oldWidgets.length; i++) {
+                // Delete old widget
+
+            }
+        }
+        else {
+            console.log("API Failure loading table '" + table.name + "'!");
+        }
+    });
+}
 
 //
 // TableTreeItem: An item in the table tree. Not necessarily a table.
@@ -37,21 +146,14 @@ function TableTreeItem(name) {
 
 function selectTableTreeItem(item) {
     if (selectedTreeItem != null) {
-        // Remove the selected class
         selectedTreeItem.label.classList.remove("selected");
     }
     selectedTreeItem = item;
     item.label.classList.add("selected");
-}
 
-function getOrAddTableTreeItem(name) {
-    if (name in treeItems)
-        return treeItems[name];
-    
-    let segments = name.split("/");
-    let newTableTreeItem = new TableTreeItem(segments[segments.length - 1]);
-    treeItems[name] = newTableTreeItem;
-    return newTableTreeItem;
+    // TODO: Delete old table contents
+    for (let i = 0; i < )
+    currentTableContents = new TableContentsControl();
 }
 
 function rebuildTableTree(tableNames) {
@@ -69,7 +171,7 @@ function rebuildTableTree(tableNames) {
                 nameSoFar += "/";
             nameSoFar += segments[i];
 
-            let item = null;// = getOrAddTableTreeItem(nameSoFar);
+            let item = null;
             if (nameSoFar in treeItems) {
                 item = treeItems[nameSoFar];
             }
@@ -113,4 +215,3 @@ function connect() {
 }
 
 connect();
-//rebuildTableTree(["a", "a2", "b", "c", "a2/q", "a2/z", "a2/w", "a2/z/2500"]);
